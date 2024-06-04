14cb9cef0de075f820ca8a22f29f1d133b286cb5
bakasifu <156412177+bakanura@users.noreply.github.com>
2024-06-04 21:08:13 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,101 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt update
+sudo apt install firewalld
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+sudo ufw disable
+sudo firewall-cmd --add-port=2381/tcp --permanent
+sudo firewall-cmd --add-port=2380/tcp --permanent
+sudo firewall-cmd --add-port=2379/tcp --permanent
+sudo firewall-cmd --add-port=10246/tcp --permanent
+sudo firewall-cmd --add-port=10250/tcp --permanent
+sudo firewall-cmd --add-port=6443/tcp --permanent
+
+# Install containerd
+curl -fLo containerd-1.6.14-linux-amd64.tar.gz \
+  https://github.com/containerd/containerd/releases/download/v1.6.14/containerd-1.6.14-linux-amd64.tar.gz
+
+## Extract the binaries
+sudo tar Cxzvf /usr/local containerd-1.6.14-linux-amd64.tar.gz
+
+## Install containerd as a service
+sudo curl -fsSLo /etc/systemd/system/containerd.service \
+  https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
+
+# Install runc
+curl -fsSLo runc.amd64 \
+  https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
+
+# Install CNI network plugins
+curl -fLo cni-plugins-linux-amd64-v1.1.1.tgz \
+https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe -a overlay br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Fetch package list
+sudo apt-get update
+
+sudo apt-get install -y kubelet kubeadm kubectl
+
+# Prevent them from being updated automatically
+sudo apt-mark hold kubelet kubeadm kubectl
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo apt-get update
+
+# See if swap is enabled
+swapon --show
+
+# Turn off swap
+sudo swapoff -a
+
+# Disable swap completely
+sudo sed -i -e '/swap/d' /etc/fstab
+
+curl -fLo cni-plugins-linux-amd64-v1.1.1.tgz \
+  https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe -a overlay br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Finally, run kubeadm for the first time
+sudo kubeadm init
