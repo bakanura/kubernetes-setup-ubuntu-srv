19e17c2fbabfb19750620e6f09efa616b14e7f11
bakasifu <156412177+bakanura@users.noreply.github.com>
2024-06-04 23:09:51 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,102 +1,87 @@
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo apt update
-sudo apt upgrade -y
-sudo apt install firewalld
-sudo systemctl enable firewalld
-sudo systemctl start firewalld
-sudo ufw disable
-sudo firewall-cmd --add-port=2381/tcp --permanent
-sudo firewall-cmd --add-port=2380/tcp --permanent
-sudo firewall-cmd --add-port=2379/tcp --permanent
-sudo firewall-cmd --add-port=10246/tcp --permanent
-sudo firewall-cmd --add-port=10250/tcp --permanent
-sudo firewall-cmd --add-port=6443/tcp --permanent
+# Enabling kernel modules (overlay and br_netfilter)
+sudo modprobe overlay
+sudo modprobe br_netfilter
 
-# Install containerd
-curl -fLo containerd-1.6.14-linux-amd64.tar.gz \
-  https://github.com/containerd/containerd/releases/download/v1.6.14/containerd-1.6.14-linux-amd64.tar.gz
-
-## Extract the binaries
-sudo tar Cxzvf /usr/local containerd-1.6.14-linux-amd64.tar.gz
-
-## Install containerd as a service
-sudo curl -fsSLo /etc/systemd/system/containerd.service \
-  https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
-
-sudo systemctl daemon-reload
-sudo systemctl enable --now containerd
-
-# Install runc
-curl -fsSLo runc.amd64 \
-  https://github.com/opencontainers/runc/releases/download/v1.1.3/runc.amd64
-sudo install -m 755 runc.amd64 /usr/local/sbin/runc
-
-# Install CNI network plugins
-curl -fLo cni-plugins-linux-amd64-v1.1.1.tgz \
-https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
-sudo mkdir -p /opt/cni/bin
-sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
-
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+# automatically load kernel modules via the config file
+sudo cat <<EOF | sudo tee /etc/modules-load.d/kubernetes.conf
 overlay
 br_netfilter
 EOF
 
-sudo modprobe -a overlay br_netfilter
+# Checking kernel module status
+lsmod | grep overlay
+lsmod | grep br_netfilter
 
-# sysctl params required by setup, params persist across reboots
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+# setting up kernel parameters via config file
+sudo cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-# Apply sysctl params without reboot
+# Applying new kernel parameters
 sudo sysctl --system
 
-# Fetch package list
+# Disabling SWAP
+
+sudo swapoff -a
+
+# checking SWAP via /procs/swaps
+cat /proc/swaps
+
+# checking SWAP via command free -m
+sudo free -m
+
+# Opening ports for Control Plane
+sudo ufw allow 6443/tcp
+sudo ufw allow 2379:2380/tcp
+sudo ufw allow 10250/tcp
+sudo ufw allow 10259/tcp
+sudo ufw allow 10257/tcp
+
+# Opening ports for Calico CNI
+sudo ufw allow 179/tcp
+sudo ufw allow 4789/udp
+sudo ufw allow 4789/tcp
+sudo ufw allow 2379/tcp
+
+sudo ufw status
+# Create two environment variables OS* and CRIO-VERSION on all your servers
+OS=xUbuntu_22.04
+CRIO_VERSION=1.28
+
+# Execute the following commands to add the cri-o repository via apt
+sudo echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /"|sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+sudo echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$CRIO_VERSION/$OS/ /"|sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$CRIO_VERSION.list
+
+## Download the gpg key
+sudo curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$CRI_VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+sudo curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+
+# Update and refresh package index
+sudo apt update
+sudo apt upgrade
+
+# Install CRI-O container runtime
+sudo apt install cri-o cri-o-runc
+
+# Add the Kube-Repo
+sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 sudo apt-get update
 
-sudo apt-get install -y kubelet kubeadm kubectl
+# installs the Kubernetes components/packages
+sudo apt install -y kubeadm kubelet kubectl
 
 # Prevent them from being updated automatically
 sudo apt-mark hold kubelet kubeadm kubectl
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-sudo apt-get update
+# Pull container images for Kubernetes beforehand
+sudo kubeadm config images pull
 
-# See if swap is enabled
-swapon --show
-
-# Turn off swap
-sudo swapoff -a
-
-# Disable swap completely
-sudo sed -i -e '/swap/d' /etc/fstab
-
-curl -fLo cni-plugins-linux-amd64-v1.1.1.tgz \
-  https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
-sudo mkdir -p /opt/cni/bin
-sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
-
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-overlay
-br_netfilter
-EOF
-
-sudo modprobe -a overlay br_netfilter
-
-# sysctl params required by setup, params persist across reboots
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
-EOF
-
-# Apply sysctl params without reboot
-sudo sysctl --system
-
-# Finally, run kubeadm for the first time
-sudo kubeadm init
+# Kubeadm init
+publicIP=192.168.0.99
+ip_address=192.168.0.99  
+cidr=172.18.0.0/16
+sudo kubeadm init --control-plane-endpoint $publicIP --apiserver-advertise-address $ip_address --pod-network-cidr=$cidr --upload-certs
