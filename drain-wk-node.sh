a89fb1d994aad6c8d522aa2cddd229749e399a29
bakasifu <156412177+bakanura@users.noreply.github.com>
2024-06-16 11:11:29 +0200

    Update drain-wk-node.sh

--- drain-wk-node.sh
+++ drain-wk-node.sh
@@ -1,8 +1,8 @@
 # kubectl drain does not work because the node will most likely contain sets.
 # The following force-command must therefore be added.
 
-kubectl drain mynodenamehere --ignore-daemonsets --delete-local-data
+kubectl drain <node_name> --ignore-daemonsets --delete-local-data
 
 # Then kubectl get nodes "SchedulingDisabled" means everything worked.
-# Now kubectl delete node "mynodenamehere"
-kubectl delete node ckawn01
+# Now kubectl delete node "<node_name>"
+kubectl delete node <node_name>
