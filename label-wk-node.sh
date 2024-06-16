5e1e4f849d2f6a69c38a48dad05e5fc057035580
bakasifu <156412177+bakanura@users.noreply.github.com>
2024-06-16 11:10:49 +0200

    Update label-wk-node.sh

--- label-wk-node.sh
+++ label-wk-node.sh
@@ -5,4 +5,3 @@ The new release v0.0.8-dev shows all roles including: worker, master, and etcd, 
 # You can manually label the node using the command below.
 
 kubectl label node <node_name> node-role.kubernetes.io/worker=worker"
-![6dca20e1e903fdc0c841df598df804df.png](:/1396d5825fbc440c814709c8cc07a7d0)
